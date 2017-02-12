ac2804bc2b27d4b0e39465e2d3f549b82d1d82cf
Cono52 <oflanac52@gmail.com>
2017-02-12 23:21:25 +0000

    concurrent start api and webpack server

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient
 
-app.listen(3000, () => {
-  console.log('listening on 3000')
+MongoClient.connect('mongodb://localhost:27017/auroraLooks', (err, database) => {
+    if (err) return console.log(err)
+    db = database
+    app.listen(3001, () => {
+        console.log('listening on 3001')
+    })
 })
 
-app.get('/', (req, res) => {
-  res.send('test')
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client/public/index.html'));
+})
+
+app.post('/looks', (req, res) => {
+  db.collection('looks').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
 })
