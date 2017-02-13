fb5059066a9f9c0f286063b43e8775f0e11feaf7
Cono52 <oflanac52@gmail.com>
2017-02-13 23:27:27 +0000

    renamed looks

--- models/looks.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var looksSchema = new mongoose.Schema({
-    id : String,
-    rating : Number,
-    skill : Number
-});
-
-module.exports = mongoose.model('Looks', looksSchema);
