d0727dfcace5338b3547bd5d020c35540da5a248
Cono52 <oflanac52@gmail.com>
2017-02-20 23:59:22 +0000

    added deps and file uplads

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,15 +1,36 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import FileInput from 'react-file-input'
 import './index.css'
 
+
+class Form extends Component {
+
+  	handleChange = (event) => {
+		console.log('Selected file:', event.target.files[0]);
+	}
+ 
+	render() {
+		return (
+				<form>
+					<FileInput name="myImage"
+							accept=".png,.gif"
+							placeholder="My Image"
+							className="inputClass"
+							onChange={this.handleChange} />
+				</form>
+		);
+	}
+}
+
 class Profile extends Component {
-    render() {
-        return (
-            <div className="Profile">
-                Profile
-                <button>Upload</button>
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className="Profile">
+				Profile
+				<Form/>
+			</div>
+		);
+	}
 }
 
 export default Profile;
