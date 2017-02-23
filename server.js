3e49fd8e2806f97ec11d9d892e428b7bcf88f354
Cono52 <oflanac52@gmail.com>
2017-02-23 22:12:01 +0000

    refactor

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const Look = require('./models/look')
 const readline = require('readline')
-
 const multer = require('multer')
-const upload = multer({dest: 'uploads/'});
+const upload = multer({
+  dest: 'uploads/'
+});
 const async = require('async')
 const _ = require('lodash')
 const easyimg = require('easyimage')
 const cv = require('opencv')
-
 const fs = require('fs')
 const app = express()
 
-var exts = {
-  'image/jpeg'   :   '.jpg',
-  'image/png'    :   '.png',
-  'image/gif'    :   '.gif'
+let exts = {
+  'image/jpeg': '.jpg',
+  'image/png': '.png',
+  'image/gif': '.gif'
 }
 
 /*db setup*/
@@ -26,56 +26,39 @@ let db = mongoose.connect('mongodb://localhost:27017/auroraLooks').connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.get('/api/looks', (req, res) => {
-  Look.find({}, function(err, looks) {
+  Look.find({}, (err, looks) => {
     if (err) throw err
-     res.json(looks)
+    res.json(looks)
   });
 })
 
-app.post('/api/checkImage', upload.single('file') , (req, res, next) => {
+app.post('/api/checkImage', upload.single('file'), (req, res, next) => {
   let filename = req.file.filename + exts[req.file.mimetype]
   let src = __dirname + '/' + req.file.path
   let dst = __dirname + './images/' + filename;
   async.waterfall(
-    [( callback ) => {
-        if (!_.contains(
-          [
-            'image/jpeg',
-            'image/png',
-            'image/gif'
-          ],
-          req.file.mimetype
-        )) {
-          return callback( new Error( 'Invalid file - please upload an image (.jpg, .png, .gif).' ) )
-        }
+    [callback => {
+        if (!_.contains(['image/jpeg', 'image/png', 'image/gif'], req.file.mimetype))
+          return callback(new Error('Invalid file'))
         return callback();
       },
-      ( callback ) => {
-        easyimg.info( src )
-        .then(file => {return callback()})
+      callback => {
+        easyimg.info(src)
+        .then(file => { return callback() })
       },
-      ( callback ) => {
-        easyimg.resize(
-          {
-            width      :   960,
-            src        :   src,
-            dst        :   dst
-          }
-        ).then(image => {
-          return callback();
-        });
+      callback => {
+        easyimg.resize({ width: 960, src: src, dst: dst })
+        .then(image => { return callback()})
       },
-      ( callback ) => { cv.readImage( dst, callback )},
-      ( im, callback ) => { im.detectObject( cv.FACE_CASCADE, {}, callback ) }
+      callback => { cv.readImage(dst, callback) },
+      (im, callback) => {im.detectObject(cv.FACE_CASCADE, {}, callback)}
     ],
-    ( err, faces ) => {
-      if ( err ) res.send({'valid': 'no'})
-
-      res.send({'faces': faces.length})
+    (err, faces) => {
+      (err) ? res.send({'valid': 'no'}): res.send({'faces': faces.length})
     }
   );
 })
@@ -108,8 +91,7 @@ let recursiveAsyncReadLine = () => {
     if (answer == 'exit') {
       console.log("closed command")
       return rl.close()
-    }
-    else if(answer.indexOf("mockpics") != -1){
+    } else if (answer.indexOf("mockpics") != -1) {
       let params = answer.split(", ")
       manualInput(params[0], params[1], params[2], params[3])
       console.log(params)
