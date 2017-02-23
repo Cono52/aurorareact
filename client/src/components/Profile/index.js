2c234b876fbd7b29a7a1d5e4a05a7c04a0abee9e
Cono52 <oflanac52@gmail.com>
2017-02-23 21:07:00 +0000

    re-did component with  a preview

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,25 +1,68 @@
 import React, {Component} from 'react'
-import FileInput from 'react-file-input'
+//import FileInput from 'react-file-input'
 import './index.css'
 
 
-class Form extends Component {
+class ImageUpload extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+		file: '',
+		imagePreviewUrl: ''
+	}
+  }
 
-  	handleChange = (event) => {
-		console.log('Selected file:', event.target.files[0]);
-	}
- 
-	render() {
-		return (
-				<form>
-					<FileInput name="myImage"
-							accept=".png,.gif"
-							placeholder="My Image"
-							className="inputClass"
-							onChange={this.handleChange} />
-				</form>
-		);
-	}
+  _handleSubmit(e) {
+    e.preventDefault()
+	let data = new FormData();
+	data.append('file' ,this.state.file)
+    fetch("api/checkImage", {method: "POST", body: data})
+	.then(res => res.json())
+	.then(res => console.log(res))
+    console.log('handle uploading-', this.state.file)
+  }
+
+  _handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
+    }
+
+    reader.readAsDataURL(file)
+  }
+
+  render() {
+    let {imagePreviewUrl} = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = (<img src={imagePreviewUrl} alt="You done messed up son"/>);
+    } else {
+      $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
+    }
+
+    return (
+      <div className="previewComponent">
+        <form onSubmit={(e)=>this._handleSubmit(e)}>
+          <input className="fileInput" 
+            type="file" 
+            onChange={(e)=>this._handleImageChange(e)} />
+          <button className="submitButton" 
+            type="submit" 
+            onClick={(e)=>this._handleSubmit(e)}>Upload Image</button>
+        </form>
+        <div className="imgPreview">
+          {$imagePreview}
+        </div>
+      </div>
+    )
+  }
 }
 
 class Profile extends Component {
@@ -27,7 +70,7 @@ class Profile extends Component {
 		return (
 			<div className="Profile">
 				Profile
-				<Form/>
+				<ImageUpload/>
 			</div>
 		);
 	}
