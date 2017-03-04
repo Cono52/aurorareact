7f9d47e367e057be7dff869c9e3b109b8a82035c
Cono52 <oflanac52@gmail.com>
2017-03-04 23:03:54 +0000

    added likes data to imgs and heart svg

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react'
-//import FileInput from 'react-file-input'
+import heart from '../heart.svg'
 import './index.css'
 
 
 class ImageUpload extends React.Component {
+
   constructor(props) {
-    super(props)
-    this.state = {
-		file: '',
-		imagePreviewUrl: ''
-	}
+      super(props)
+      this.state = {
+      file: '',
+      imagePreviewUrl: ''
+    }
   }
 
   _handleSubmit(e) {
@@ -71,15 +72,15 @@ class Profile extends Component {
      super(props);
      this.state = {
        imgLinks: [
-         "http://3.bp.blogspot.com/-Kvf4OML7qa0/VMFQMVWuFqI/AAAAAAAAMsE/71S40JFXYrQ/s1600/IMG_7044.JPG",
-         "https://1.bp.blogspot.com/-AQ9v4zAyzxg/WEIbSbCWpyI/AAAAAAAATME/oFFSdas3BsAB4lkKyJuWm3naPn__MJeywCLcB/s600/15178303_1464398510256560_1732442245886320636_n-2.jpg",
-         "https://3.bp.blogspot.com/-yN2wRud9syo/WBFtb4KRTZI/AAAAAAAAS4c/KSessKJb4FoWzDwPTZK64yQaambKMBzmQCLcB/s600/IMG_2106.JPG",
-         "https://3.bp.blogspot.com/-qtBTlAVbK1k/Vx1JRC4Vg8I/AAAAAAAAQaM/H5-YXlZdoJUJv1vzDV0eKn4Qz4Ux2DzgQCLcB/s1600/IMG_8031.JPG",
-         "https://1.bp.blogspot.com/-RCj1lme_DIg/Vlt-x4GR3lI/AAAAAAAAQBk/iWzgFKBFVgU/s600/12314054_1181942851835462_4589169900682272178_n-3.jpg",
-         "https://4.bp.blogspot.com/-J46DCOqGcRc/VlDE9KBh7WI/AAAAAAAAP-0/zkR8oowL39k/s600/12274639_1177376942292053_1520249981167398755_n.jpg",
-         "https://1.bp.blogspot.com/-YINIM6j8zIM/VjqsrniuHzI/AAAAAAAAP40/Mai5ThErItA/s600/IMG_2127.JPG",
-         "https://2.bp.blogspot.com/-zhajdAvWnUM/Vi46YyAvQqI/AAAAAAAAP28/WfZi8wHBl1k/s600/11220074_1165205506842530_7531581878298417072_n.jpg",
-         "https://2.bp.blogspot.com/-93S7FcAuM-I/VgR2f6lqcsI/AAAAAAAAPmM/YhCStMVcVRo/s600/12042627_1150400111656403_6912228186095638186_n.jpg"
+         { src: "http://3.bp.blogspot.com/-Kvf4OML7qa0/VMFQMVWuFqI/AAAAAAAAMsE/71S40JFXYrQ/s1600/IMG_7044.JPG", like: "43"},
+         { src: "https://1.bp.blogspot.com/-AQ9v4zAyzxg/WEIbSbCWpyI/AAAAAAAATME/oFFSdas3BsAB4lkKyJuWm3naPn__MJeywCLcB/s600/15178303_1464398510256560_1732442245886320636_n-2.jpg", like: "2"},
+         { src: "https://3.bp.blogspot.com/-yN2wRud9syo/WBFtb4KRTZI/AAAAAAAAS4c/KSessKJb4FoWzDwPTZK64yQaambKMBzmQCLcB/s600/IMG_2106.JPG", like: "133"},
+         { src: "https://3.bp.blogspot.com/-qtBTlAVbK1k/Vx1JRC4Vg8I/AAAAAAAAQaM/H5-YXlZdoJUJv1vzDV0eKn4Qz4Ux2DzgQCLcB/s1600/IMG_8031.JPG", like: "263"},
+         { src: "https://1.bp.blogspot.com/-RCj1lme_DIg/Vlt-x4GR3lI/AAAAAAAAQBk/iWzgFKBFVgU/s600/12314054_1181942851835462_4589169900682272178_n-3.jpg", like: "432"},
+         { src: "https://4.bp.blogspot.com/-J46DCOqGcRc/VlDE9KBh7WI/AAAAAAAAP-0/zkR8oowL39k/s600/12274639_1177376942292053_1520249981167398755_n.jpg", like: "4564"},
+         { src: "https://1.bp.blogspot.com/-YINIM6j8zIM/VjqsrniuHzI/AAAAAAAAP40/Mai5ThErItA/s600/IMG_2127.JPG", like: "787"},
+         { src: "https://2.bp.blogspot.com/-zhajdAvWnUM/Vi46YyAvQqI/AAAAAAAAP28/WfZi8wHBl1k/s600/11220074_1165205506842530_7531581878298417072_n.jpg", like: "76"},
+         { src: "https://2.bp.blogspot.com/-93S7FcAuM-I/VgR2f6lqcsI/AAAAAAAAPmM/YhCStMVcVRo/s600/12042627_1150400111656403_6912228186095638186_n.jpg", like: "98"}
        ]
      }
    }
@@ -87,7 +88,11 @@ class Profile extends Component {
    populateGrid() {
      return this.state.imgLinks.map( image => {
         return <div key={this.state.imgLinks.indexOf(image)} className="gridImg">
-                  <img src={image} alt=""/>
+                  <img src={image.src} alt=""/>
+                  <div className="stats">
+                    <img src={heart} alt=""/>
+                    <p>{image.like}</p>
+                  </div>
                </div>  
      })
    }
